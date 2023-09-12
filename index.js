e3a46e76e89c8518f02c37ad76518e652f3c89e5
Artem Tkachik <artemtkachik94@gmail.com>
2023-09-12 11:23:48 +0200

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+import { pizza4Cheese } from "./PizzaAbstractFactory.js";
+import { pizzaPepperoni } from "./PizzaBuilder.js";
+import { pizzaSarda } from "./PizzaFactoryMethod.js";
+
+pizza4Cheese.toTaste();
+pizzaSarda.toTaste();
+pizzaPepperoni.toTaste();
